b2ebfca338a86a8e6f440c834eba973a4d30ede4
Erik <aultman.03@gmail.com>
2018-11-26 14:43:32 -0500

    Initial Commit

---
+++ Clockwork.API/Controllers/CurrrentTimeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Clockwork.API.Models;
+using System.Collections.Generic;
+
+namespace Clockwork.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CurrentTimeController : Controller
+    {
+        // GET api/currenttime
+        [HttpGet]
+        [Route("")]
+        public IActionResult Get()
+        {
+            var utcTime = DateTime.UtcNow;
+            var serverTime = DateTime.Now;
+            var ip = this.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            return Ok(SaveTime(utcTime, serverTime, ip));
+        }
+
+        // GET api/currenttime/all
+        [HttpGet]
+        [Route("all")]
+        public IActionResult GetAll()
+        {
+            var returnList = new List<CurrentTimeQuery>();
+
+            using (var db = new ClockworkContext())
+            {
+                foreach (var CurrentTimeQuery in db.CurrentTimeQueries)
+                {
+                    returnList.Add(new CurrentTimeQuery
+                    {
+                        CurrentTimeQueryId = CurrentTimeQuery.CurrentTimeQueryId,
+                        Time = CurrentTimeQuery.Time,
+                        ClientIp = CurrentTimeQuery.ClientIp,
+                        UTCTime = CurrentTimeQuery.UTCTime
+                    });
+                }
+            }
+
+            return Ok(returnList);
+        }
+
+        // GET api/currenttime/{timeZone}
+        [HttpGet]
+        [Route("{timeZone}")]
+        public IActionResult GetByTimeZone(string timeZone)
+        {
+            TimeZoneInfo tz = TimeZoneInfo.FindSystemTimeZoneById(timeZone);
+            DateTime dateTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, tz);
+
+            var utcTime = DateTime.UtcNow;
+            var serverTime = dateTime;
+            var ip = this.HttpContext.Connection.RemoteIpAddress.ToString();
+            
+
+            return Ok(SaveTime(utcTime, serverTime, ip));
+        }
+
+        private CurrentTimeQuery SaveTime(DateTime utcTime, DateTime serverTime, string ip)
+        {
+            var returnVal = new CurrentTimeQuery
+            {
+                UTCTime = utcTime,
+                ClientIp = ip,
+                Time = serverTime
+            };
+
+            using (var db = new ClockworkContext())
+            {
+                db.CurrentTimeQueries.Add(returnVal);
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+                foreach (var CurrentTimeQuery in db.CurrentTimeQueries)
+                {
+                    Console.WriteLine(" - {0}", CurrentTimeQuery.UTCTime);
+                }
+            }
+            return returnVal;
+        }
+    }
+}
---
+++ Clockwork.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace Clockwork.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var mvcName = typeof(Controller).Assembly.GetName();
+            var isMono = Type.GetType("Mono.Runtime") != null;
+
+            ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+            ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+            var timeZones = new List<string>();
+            foreach (TimeZoneInfo zone in TimeZoneInfo.GetSystemTimeZones())
+                timeZones.Add(zone.Id);
+            ViewData["TimeZones"] = timeZones;
+            return View();
+        }
+    }
+}
